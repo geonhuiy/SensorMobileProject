b767c1da8b66840be67589226d398255ebad2119
Jamie Yoon <rkawk3316@gmail.com>
2019-09-26 18:17:12 +0300

    ADD: Floating action button to take pictures

--- FunPlus/app/src/main/java/com/example/funplus/MainActivity.kt
+++ FunPlus/app/src/main/java/com/example/funplus/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.funplus
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 const val TAG = "DBG"
 class MainActivity : AppCompatActivity() {
-    lateinit var plusMinusFrag: PlusMinusFrag
-    lateinit var letterFrag: LetterFrag
-    lateinit var sosFrag: SosFrag
-    lateinit var fTransaction: FragmentTransaction
-    lateinit var fManager: FragmentManager
+    private lateinit var plusMinusFrag: PlusMinusFrag
+    private lateinit var letterFrag: LetterFrag
+    private lateinit var sosFrag: SosFrag
+    private lateinit var fTransaction: FragmentTransaction
+    private lateinit var fManager: FragmentManager
+
+    private val captureReq = 0
+    private var photoPath: String = ""
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +46,9 @@ class MainActivity : AppCompatActivity() {
         sosBtn.setOnClickListener {
             goToGameFrag(sosFrag)
         }
+        fab_sos.setOnClickListener {
+            takePicture()
+        }
     }
 
 
@@ -52,5 +67,34 @@ class MainActivity : AppCompatActivity() {
         //fTransaction.addToBackStack(null)
         fTransaction.commit()
     }
+    /*
+     *Picture
+     */
+    private fun takePicture() {
+        //File name and path
+        val filename = "sosImg"
+        val imgPath = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        var imgFile: File? = null
+        imgFile = File.createTempFile(filename, ".jpg", imgPath)
+        photoPath = imgFile!!.absolutePath
+        val photoUri: Uri = FileProvider.getUriForFile(this, "com.domain.fileprovider", imgFile)
+
+        val captureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        //Starts camera activity
+        if(captureIntent.resolveActivity(packageManager)!= null) {
+            captureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+            startActivityForResult(captureIntent, captureReq)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == captureReq && resultCode == Activity.RESULT_OK) {
+            //Gets a bitmap from picture taken with camera
+            val imgBitmap = BitmapFactory.decodeFile(photoPath)
+            Log.d("Photopath", photoPath)
+        }
+    }
+
 
 }
---
+++ FunPlus/app/src/main/java/com/example/funplus/Photo.kt
@@ -0,0 +1,5 @@
+package com.example.funplus
+
+class Photo {
+
+}
