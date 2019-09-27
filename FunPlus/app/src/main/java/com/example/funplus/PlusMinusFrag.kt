1bc78f5ef6325a051a6ec99753dd051980d5ad3f
YouqinS <youqin.sun@metropolia.fi>
2019-09-27 22:34:46 +0300

    improve UI: colors, fonts use RadioButton for user to choose answer

--- FunPlus/app/src/main/java/com/example/funplus/PlusMinusFrag.kt
+++ FunPlus/app/src/main/java/com/example/funplus/PlusMinusFrag.kt
@@ -1,10 +1,12 @@
 package com.example.funplus
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -21,7 +23,8 @@ class PlusMinusFrag() : Fragment() {
     lateinit var fTransaction: FragmentTransaction
     lateinit var fManager: FragmentManager
     lateinit var gameDB: GameDB
-    private var gameId = 0
+    private var correctAnswer = 0
+    private var userAnswer = 0
     var toRepeatGame = false
     var gameStarted = false
     lateinit var newGame: Game
@@ -29,9 +32,7 @@ class PlusMinusFrag() : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
-
+        arguments?.let { }
     }
 
     override fun onCreateView(
@@ -55,29 +56,17 @@ class PlusMinusFrag() : Fragment() {
 
         /*set game on the UI */
         setGame()
-
         okBtn.setOnClickListener {
-            val correctAnswer = getAnswer()
-            val answerTxt = answerEt.text.toString()
-            if (answerTxt.isEmpty() || answerTxt.equals("")) {
-                Toast.makeText(this.context, "Input a number", Toast.LENGTH_LONG).show()
+            Log.d(TAG + "userAnswer:", userAnswer.toString())
+            Log.d(TAG + "correctAnswer:", correctAnswer.toString())
+            if (userAnswer == correctAnswer) {
+                insertOrUpdateGameDB(newGame, true)
+                goToAnswerFrag(correctAnswerFrag)
             } else {
-                try {
-                    val answer = answerTxt.toInt()
-                    if (answer == correctAnswer) {
-                        insertOrUpdateGameDB(newGame, true)
-                        goToAnswerFrag(correctAnswerFrag)
-                    } else {
-                        insertOrUpdateGameDB(newGame, false)
-                        goToAnswerFrag(wrongAnswerFrag)
-                    }
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                    Log.d(TAG, e.toString())
-                }
+                insertOrUpdateGameDB(newGame, false)
+                goToAnswerFrag(wrongAnswerFrag)
             }
         }
-
     }
 
     /*set a game on the UI, if user wants to redo a game,
@@ -96,22 +85,6 @@ class PlusMinusFrag() : Fragment() {
         }
     }
 
-    /*create a new game with two randowm numbers from 1 to 10
-     * and random sign of "+" or "-"
-     */
-    private fun createNewGame() {
-        Log.d(TAG, "set new numbers")
-        var num1 = (1..10).random()
-        var num2 = (1..10).random()
-        val sign = mutableListOf<String>("+", "-").random()
-        if (sign.equals("-") && num1 < num2) {
-            val tmp = num1
-            num1 = num2
-            num2 = tmp
-        }
-        setGameOnUI(Game(num1, num2, sign))
-    }
-
     /*check if the game is stored in db already
      *if yes, update the winCount/loseCount
      * if not, insert a new game into db
@@ -125,11 +98,17 @@ class PlusMinusFrag() : Fragment() {
 
                 //update existing game info
                 if (isAnswerCorrect) {
-                    Log.d(TAG + "getWinCount", gameDB.gameDao().getWinCount(gameId).toString())
+                    Log.d(
+                        TAG + "getWinCount(before update)",
+                        gameDB.gameDao().getWinCount(gameId).toString()
+                    )
                     gameDB.gameDao().updateWinCount(gameId)
                     Log.d(TAG + "getWinCount", gameDB.gameDao().getWinCount(gameId).toString())
                 } else {
-                    Log.d(TAG + "getLoseCount", gameDB.gameDao().getLoseCount(gameId).toString())
+                    Log.d(
+                        TAG + "getLoseCount(before update)",
+                        gameDB.gameDao().getLoseCount(gameId).toString()
+                    )
                     gameDB.gameDao().updateLoseCount(gameId)
                     Log.d(TAG + "getLoseCount", gameDB.gameDao().getLoseCount(gameId).toString())
                 }
@@ -138,9 +117,9 @@ class PlusMinusFrag() : Fragment() {
                 //create a new game
                 Log.d(TAG, "game not in DB")
                 if (isAnswerCorrect) {
-                    gameId = gameDB.gameDao().insert(GameData(0, game, 1, 0, Date())).toInt()
+                    gameDB.gameDao().insert(GameData(0, game, 1, 0, Date())).toInt()
                 } else {
-                    gameId = gameDB.gameDao().insert(GameData(0, game, 0, 1, Date())).toInt()
+                    gameDB.gameDao().insert(GameData(0, game, 0, 1, Date())).toInt()
                 }
             }
         }
@@ -162,32 +141,51 @@ class PlusMinusFrag() : Fragment() {
         return foundGameInDB
     }
 
+    /*create a new game with two randowm numbers from 1 to 10
+     * and random sign of "+" or "-"
+     */
+    private fun createNewGame() {
+        Log.d(TAG, "set new numbers")
+        var num1 = (1..10).random()
+        var num2 = (1..10).random()
+        val sign = mutableListOf<String>("+", "-").random()
+        if (sign.equals("-") && num1 < num2) {
+            val tmp = num1
+            num1 = num2
+            num2 = tmp
+        }
+        newGame = Game(num1, num2, sign)
+        setGameOnUI(newGame)
+    }
+
     /*if user wants to repeat a game
      * fetch the previous/last game from db
      * and set it on UI
      */
     private fun repeatGame() {
         Log.d(TAG, "repeatGame")
-        var previousGame: Game? = null
         val gameModel = ViewModelProviders.of(this).get(GameModel::class.java)
         doAsync {
             Log.d(TAG, gameModel.getLastGame().toString())
-            previousGame = gameModel.getLastGame().game
+            val previousGame = gameModel.getLastGame().game
             Log.d(TAG, previousGame.toString())
 
             uiThread {
-                setGameOnUI(previousGame!!)
+                newGame = previousGame
+                setGameOnUI(newGame)
             }
         }
     }
 
     //set the given game on UI
     private fun setGameOnUI(game: Game) {
-        newGame = game
         Log.d(TAG, "setGameOnUI: " + game.num1 + game.sign + game.num2)
         num1Btn.text = game.num1.toString()
         num2Btn.text = game.num2.toString()
         signBtn.text = game.sign
+        getCorrectAnswer()
+        setAnswerOptions()
+        getUserAnswer()
     }
 
     /*navigate to answer fragment based on user's answer
@@ -203,17 +201,52 @@ class PlusMinusFrag() : Fragment() {
     }
 
     /*calculate the correct answer of the game shown on UI */
-    private fun getAnswer(): Int {
-        val num1 = num1Btn.text.toString().toInt()
-        val num2 = num2Btn.text.toString().toInt()
-        val sign = signBtn.text.toString()
-        var answer = 0
-        if (sign.equals("+")) {
-            answer = num1 + num2
+    private fun getCorrectAnswer() {
+        if (newGame.sign.equals("+")) {
+            correctAnswer = newGame.num1 + newGame.num2
         } else {
-            answer = num1 - num2
+            correctAnswer = newGame.num1 - newGame.num2
         }
-        Log.d(TAG, "getAnswer: " + num1 + sign + num2 + " = " + answer)
-        return answer
+        Log.d(
+            TAG,
+            "getCorrectAnswer: " + newGame.num1 + newGame.sign + newGame.num2 + " = " + correctAnswer
+        )
+    }
+
+    private fun setAnswerOptions() {
+        val num1: Int
+        val num2: Int
+        when (correctAnswer) {
+            0 -> {
+                num1 = (1..10).random()
+                num2 = (11..20).random()
+            }
+            20 -> {
+                num1 = (0..10).random()
+                num2 = (11..19).random()
+            }
+            else -> {
+                num1 = (0 until correctAnswer).random()
+                num2 = ((correctAnswer + 1)..20).random()
+            }
+        }
+        val options = mutableListOf<Int>(correctAnswer, num1, num2)
+        val newOptions = options.shuffled()
+        radioButton1.text = newOptions[0].toString()
+        radioButton2.text = newOptions[1].toString()
+        radioButton3.text = newOptions[2].toString()
+    }
+
+
+    private fun getUserAnswer() {
+        radioGroup.clearCheck()
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            val rb: RadioButton = group.findViewById(checkedId)
+            if (checkedId > -1) {
+                Toast.makeText(this.context, rb.text, Toast.LENGTH_SHORT).show()
+                userAnswer = rb.text.toString().toInt()
+            }
+        }
+        Log.d(TAG + "getUserAnswer: ", userAnswer.toString())
     }
 }
